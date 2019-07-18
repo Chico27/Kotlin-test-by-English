cfe44b8b06155a5b07adc900572de3096b508cb4
chico27 <masamasa.987654321@gmail.com>
2019-07-18 21:05:00 +0900

    new

--- HelloKotlin/src/Main.kt
+++ HelloKotlin/src/Main.kt
@@ -1,3 +1,21 @@
+
+fun getGreeting(): String {
+    return "Hello Kotlin"
+}
+
+fun sayHello(): Unit {
+    println(getGreeting())
+}
+
+fun sayHello(itemToGreet:String){
+    val msg = "Hello " + itemToGreet
+    println(msg)
+}
+
+
 fun main() {
-    println("Hello Kotlin")
+    println("Hello World")
+    println(getGreeting())
+    sayHello()
+    sayHello("Kotlin")
 }
